d8c8310f61fb6929d2308f61e76de4973d78e123
DS5AJD <81415671+ds5ajd@users.noreply.github.com>
2024-01-22 17:51:11 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -179,7 +179,7 @@ class CarInterface(CarInterfaceBase):
 
     # longitudinal
     ret.longitudinalTuning.kpBP = [0., 18.0 * CV.KPH_TO_MS, 80.0 * CV.KPH_TO_MS] #[0., 18.0 * CV.KPH_TO_MS, 36.0 * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.10, 1.0, 0.76] #[1.20, 1.0, 0.76], [1.16, 1.1, 0.76]=>good ==>[1.18, 1.17, 1.0, 0.76]
+    ret.longitudinalTuning.kpV = [1.10, 0.97, 0.76] #[1.20, 1.0, 0.76], [1.16, 1.1, 0.76]=>good ==>[1.18, 1.17, 1.0, 0.76]
     ret.longitudinalTuning.kiBP = [0., 18.0 * CV.KPH_TO_MS, 80.0 * CV.KPH_TO_MS] #[0., 18.0 * CV.KPH_TO_MS, 36.0 * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS]
     ret.longitudinalTuning.kiV =  [0.140, 0.125, 0.115] #[0.140,0.125,0.115], [0.135, 0.130, 0.115]=> good ==>[0.140, 0.135, 0.130, 0.115]
      
