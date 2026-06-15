a877657ed1b706ba085ba67c43fd20f5d8b2ca42
DS5AJD <81415671+ds5ajd@users.noreply.github.com>
2026-06-15 23:22:45 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -92,11 +92,13 @@ class CarInterface(CarInterfaceBase):
     ret.restartForceAccel = Params().get_bool('RestartForceAccel')
     ret.openpilotLongitudinalControl = ret.enableGasInterceptor
 
+
+    
     tire_stiffness_factor = 0.5
 
     ret.minSteerSpeed = 11 * CV.KPH_TO_MS
     ret.steerRateCost = 0.45 # 0.45 very good 0.35(origin) # def : 2.0
-    ret.steerActuatorDelay = 0.00 #0.01  # def: 0.2 Default delay, not measured yet
+    ret.steerActuatorDelay = 0.0 #0.01  # def: 0.2 Default delay, not measured yet
 
     ret.minEnableSpeed = -1
     ret.mass = 1625. + STD_CARGO_KG
@@ -142,14 +144,16 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = 2.0828 #ret.wheelbase * 0.4 # wild guess
       tire_stiffness_factor = 1.0
       # still working on improving lateral
-      ret.steerRateCost = 0.5 # 0.3 default 0.5
-      ret.steerActuatorDelay = 0. # 0.2 default 0.
+      ret.steerRateCost = 0.45 # 0.3 default 0.5
+      ret.steerActuatorDelay = 0.25 # 0.2 default 0.
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[10., 41.0], [10., 41.0]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.14, 0.24], [0.01, 0.021]]
       ret.lateralTuning.pid.kdBP = [0.]
       ret.lateralTuning.pid.kdV = [0.5]
       ret.lateralTuning.pid.kf = 1. # for get_steer_feedforward_bolt()
-      
+
+
+    
       
     else:
       ret.lateralTuning.init('torque')
